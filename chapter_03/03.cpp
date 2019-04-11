310fca74e35913773712c5e867f4669d891c4f13
chen.ji <chen.ji@pica8.com>
2019-04-11 15:31:32 +0800

    update chapter 3 exercises

--- chapter_03/03.cpp
+++ chapter_03/03.cpp
@@ -1,14 +1,32 @@
-#include <cctype>
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-int main(void) {
+void test_string() {
     const string s = "Keep out!";
     for (auto &c: s) {
         cout << c << endl;
     }
+    return;
+}
 
+vector<int>& test_vector(vector<int> &vx) {
+    const vector<int> v0 = {1, 2, 3, 4, 5};
+    for (auto it0 = v0.begin(); it0 != v0.end(); ++it0) {
+        vx.push_back(*it0);
+    }
+    return vx;
+}
+
+vector<int> vx = {0, 99, 98, -100};
+
+int main(void) {
+    test_string();
+    auto v = test_vector(vx);
+    for (auto i : v) {
+        cout << i << endl;
+    }
     return 0;
 }
