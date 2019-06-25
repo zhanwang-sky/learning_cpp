272234b47c0559e0ee7d401163346761c41acb0f
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2019-06-25 21:05:40 +0800

    fix build errors

--- chapter_16/main.cpp
+++ chapter_16/main.cpp
@@ -1,4 +1,3 @@
-#include <cstring>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -11,34 +10,39 @@ using std::cout;
 using std::endl;
 using std::string;
 using std::vector;
+using util::print_array1;
+using util::print_array2;
+using util::print_array3;
+using util::get_size;
+using util::size_of;
 
 vector<int> vi = { 1, 2, 3, 4, 5 };
 vector<string> vs = { "hello", "wonderful", "world" };
 char cstr[] = "I am the king of the world";
 
 int main(void) {
-    cout << compare(1, 2) << endl;
-    cout << compare('x', 'y') << endl;
-    cout << compare(string("1"), string("12")) << endl;
+    cout << util::compare(1, 2) << endl;
+    cout << util::compare('x', 'y') << endl;
+    cout << util::compare(string("1"), string("12")) << endl;
 
     cout << endl; //////////
 
-    auto vi_it = find(vi.begin(), vi.end(), 5);
+    auto vi_it = util::find(vi.begin(), vi.end(), 5);
     if (vi_it != vi.end()) {
         cout << *vi_it << endl;
     } else {
         cout << "\'5\' not found in vi\n";
     }
 
-    auto vs_it = find(vs.begin(), vs.end(), "nihao");
+    auto vs_it = util::find(vs.begin(), vs.end(), "nihao");
     if (vs_it != vs.end()) {
         cout << *vs_it << endl;
     } else {
         cout << "\'nihao\' not found in vs\n";
     }
 
-    auto cstr_it = find(begin(cstr), end(cstr), 'k');
-    if (cstr_it != end(cstr)) {
+    auto cstr_it = util::find(util::begin(cstr), util::end(cstr), 'k');
+    if (cstr_it != util::end(cstr)) {
         cout << *cstr_it << endl;
     } else {
         cout << "\'k\' not found in cstr\n";
@@ -53,7 +57,7 @@ int main(void) {
 
     cout << endl; //////////
 
-    cout << "end(cstr) - begin(cstr) = " << end(cstr) - begin(cstr) << endl;
+    cout << "end(cstr) - begin(cstr) = " << util::end(cstr) - util::begin(cstr) << endl;
     vi.push_back(6);
     cout << "size of vi = " << get_size(vi) << endl;
     cout << "size of cstr = " << size_of(cstr) << endl;
--- chapter_16/util.h
+++ chapter_16/util.h
@@ -3,55 +3,61 @@
 
 #include <ostream>
 
-template<typename T>
-int compare(const T &v1, const T &v2) {
-    if (v1 < v2) { return -1; }
-    else if (v2 < v1) { return 1; }
-    else { return 0; }
-}
+namespace util {
 
-template<typename InputIt, typename T>
-InputIt find(InputIt first, InputIt last, const T &value) {
-    for (; first != last; ++first) {
-        if (*first == value) {
-            break;
+    template<typename T>
+    int compare(const T &v1, const T &v2) {
+        if (v1 < v2) { return -1; }
+        else if (v2 < v1) { return 1; }
+        else { return 0; }
+    }
+
+    template<typename InputIt, typename T>
+    InputIt find(InputIt first, InputIt last, const T &value) {
+        for (; first != last; ++first) {
+            if (*first == value) {
+                break;
+            }
         }
+        return first;
     }
-    return first;
-}
 
-template<typename T>
-void print_array1(const T &arr) {
-    for (auto i : arr) {
-        std::cout << i;
+    template<typename T>
+    void print_array1(const T &arr) {
+        for (auto i : arr) {
+            std::cout << i;
+        }
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
-}
 
-template<typename T, unsigned N>
-void print_array2(const T (&arr)[N]) {
-    for (auto i : arr) {
-        std::cout << i;
+    template<typename T, unsigned N>
+    void print_array2(const T (&arr)[N]) {
+        for (auto i : arr) {
+            std::cout << i;
+        }
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
-}
 
-template<typename T, unsigned N>
-void print_array3(const T (*arr)[N]) {
-    for (auto i : *arr) {
-        std::cout << i;
+    template<typename T, unsigned N>
+    void print_array3(const T (*arr)[N]) {
+        for (auto i : *arr) {
+            std::cout << i;
+        }
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
-}
 
-template<class T, std::size_t N>
-constexpr T* begin(T (&arr)[N]) noexcept { return (T*) &arr; }
-template<class T, std::size_t N>
-constexpr T* end(T (&arr)[N]) noexcept { return ((T*) &arr) + N; }
+    template<class T, std::size_t N>
+    constexpr T* begin(T (&arr)[N]) noexcept { return (T*) &arr; }
 
-template<typename C>
-inline auto get_size(const C &c) -> decltype(c.size()) { return c.size(); }
-template<class T, std::size_t N>
-constexpr std::size_t size_of(const T (&arr)[N]) noexcept { return N; }
+    template<class T, std::size_t N>
+    constexpr T* end(T (&arr)[N]) noexcept { return ((T*) &arr) + N; }
+
+    template<typename C>
+    inline auto get_size(const C &c) -> decltype(c.size()) { return c.size(); }
+
+    template<class T, std::size_t N>
+    constexpr std::size_t size_of(const T (&arr)[N]) noexcept { return sizeof(arr); }
+
+}; // namespace util
 
 #endif // _UTIL_H
